25899d233d33fa262322acdcf2a058f96c621331
Bala Clark <balaclark@gmail.com>
2015-09-27 19:56:43 +0100

    starting ubuntu setup script

---
+++ ubuntu.sh
@@ -0,0 +1,6 @@
+sudo apt-get remove -y unity-lens-shopping
+sudo apt-get install -y unity-tweak-tool chromium-browser ubuntu-restricted-extras caffeine xclip
+sudo /usr/share/doc/libdvdread4/install-css.sh
+sudo apt-get install -y git vim-nox
+sudo apt-get autoremove -y
+sudo apt-get install unity-webapps-service
