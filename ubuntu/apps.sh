2d42731aa01b2349baecf7d92fab44e46548bf73
Bala Clark <balaclark@gmail.com>
2015-09-28 17:44:23 +0100

    more apps

--- ubuntu/apps.sh
+++ ubuntu/apps.sh
@@ -11,6 +11,7 @@ sudo apt-get install -y \
   git \
   tig \
   gitg \
+  meld \
   vim-nox \
   zsh \
   tmux \
@@ -21,4 +22,12 @@ sudo /usr/share/doc/libdvdread4/install-css.sh
 
 sudo apt-get autoremove -y
 sudo apt-get install unity-webapps-service
+
+# scudcloud (slack)
+sudo apt-add-repository -y ppa:rael-gc/scudcloud
+sudo apt-get update
+sudo apt-get install -y scudcloud
+#sudo dpkg-divert --add --rename --divert /opt/scudcloud/resources/scudcloud.png.real /opt/scudcloud/resources/scudcloud.png
+#sudo cp ~/scudcloud.png /opt/scudcloud/resources/
+#sudo chmod +r /opt/scudcloud/resources/scudcloud.png
 
