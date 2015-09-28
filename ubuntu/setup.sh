2bca8d40ba28c5a5839a19d4634d08453f3d1208
Bala Clark <balaclark@gmail.com>
2015-09-28 08:58:07 +0100

    numix theme

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -7,4 +7,5 @@ source $DIR/../zsh.sh
 source $DIR/../dotfiles.sh
 source $DIR/../node.sh
 source $DIR/../mongo.sh
+source $DIR/themes.sh
 
---
+++ ubuntu/themes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Numix theme
+#sudo add-apt-repository ppa:numix/ppa
+#sudo apt-get update
+#sudo apt-get install -y numix-*
+
+gsettings set org.gnome.desktop.interface gtk-theme "Numix"
+gsettings set org.gnome.desktop.wm.preferences theme "Numix"
+gsettings set org.gnome.desktop.interface icon-theme "Numix-circle"
+
+# Fonts
+sudo apt-get install -y fonts-inconsolata
