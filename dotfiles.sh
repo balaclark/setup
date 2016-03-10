aedcf18da6b456ef7edc77c294360aa2463ba26a
Bala Clark <balaclark@gmail.com>
2016-03-10 11:06:08 +0000

    install powerline fonts

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,6 +4,9 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# install patched powerline fonts
+curl https://raw.githubusercontent.com/powerline/fonts/master/install.sh | bash
+
 # setup dotfiles
 mkdir -p ~/dev
 cd ~/dev
